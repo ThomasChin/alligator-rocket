7a70fe5d8f44d0ee97008c9eddcb706b7c0371b0
ThomasChin <tchin10@outlook.com>
2019-02-03 20:52:45 -0800

    Update navigation system and remove extra page

--- Crawl/Crawl/Views/MainPage.xaml.cs
+++ Crawl/Crawl/Views/MainPage.xaml.cs
@@ -1,15 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Crawl.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainPage : TabbedPage
 	{
 		public MainPage ()
 		{
 			InitializeComponent ();
 		}
-	}
+    }
 }
