d95c28790756fc877b419df7d3c7f113ad227ea5
ThomasChin <tchin10@outlook.com>
2019-02-11 21:42:24 -0800

    Add UX to MonsterNewPage and add basic functionality to code-behind

--- Crawl/Crawl/Views/Monsters/MonsterNewPage.xaml.cs
+++ Crawl/Crawl/Views/Monsters/MonsterNewPage.xaml.cs
@@ -26,15 +26,32 @@ namespace Crawl.Views
             BindingContext = this;
         }
 
+        // Save Monster to db and redirect to Monster Index
         private async void Save_Clicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "AddData", Data);
             await Navigation.PopAsync();
         }
 
+        // Cancel Monster Creation and redirect to Monster Index
         private async void Cancel_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
+        }
+        // Picker functionality for Monster type selection
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*Picker picker = (Picker)sender;
+
+            if (picker.SelectedIndex == 0)
+                Data.Class = new KnightClass();
+
+            if (picker.SelectedIndex == 1)
+                Data.Class = new MageClass();
+
+            if (picker.SelectedIndex == 2)
+                Data.Class = new AssasinClass();
+                */               
         }
     }
 }
