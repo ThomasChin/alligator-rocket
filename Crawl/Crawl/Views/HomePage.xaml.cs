c4268c8b3114d045f3a9b56f2674c0470791fcbf
ThomasChin <tchin10@outlook.com>
2019-02-19 20:13:18 -0800

    Add buttons for Battle Modes

--- Crawl/Crawl/Views/HomePage.xaml.cs
+++ Crawl/Crawl/Views/HomePage.xaml.cs
@@ -7,9 +7,16 @@ namespace Crawl.Views
 {
     public partial class HomePage : ContentPage
     {
+        // Home Page Constructor
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        // Open About Page
+        private async void AboutButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutPage());
         }
     }
 }
