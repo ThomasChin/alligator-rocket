12407f8b4c3c680815eba2754204acd895f7df72
ThomasChin <tchin10@outlook.com>
2019-02-22 11:22:26 -0800

    Get working skeleton for AutoBattle Engine and hook up XAML

---
+++ Crawl/Crawl/GameEngine/AutoBattleEngine.cs
@@ -0,0 +1,85 @@
+﻿using Crawl.Models;
+using System.Diagnostics;
+
+namespace Crawl.GameEngine
+{
+    class AutoBattleEngine
+    {
+        public BattleEngine BattleEngine = new BattleEngine();
+
+        // Automated Battle Sequence.
+        public bool ExecuteAutoBattle()
+        {
+            // Picks 6 Characters
+            if (BattleEngine.AddCharactersToBattle() == false)
+            {
+                // Error, so exit...
+                return false;
+            }
+
+            // Start
+            BattleEngine.StartBattle(true);
+
+            Debug.WriteLine("Battle Start" + " Characters :" + BattleEngine.CharacterList.Count);
+
+            // Initialize the Rounds
+            BattleEngine.StartRound();
+
+            // Enum for RoundResult
+            RoundEnum RoundResult;
+
+            // Fight Loop. Continue until Game is Over...
+            do
+            {
+                // Do the turn...
+                RoundResult = BattleEngine.RoundNextTurn();
+
+                // If the round is over start a new one...
+                if (RoundResult == RoundEnum.NewRound)
+                {
+                    BattleEngine.NewRound();
+                    Debug.WriteLine("New Round :" + BattleEngine.BattleScore.RoundCount);
+                }
+
+            } while (RoundResult != RoundEnum.GameOver);
+
+            BattleEngine.EndBattle();
+
+            return true;
+        }
+
+        // Get current score value.
+        public int GetScoreValue()
+        {
+            return BattleEngine.BattleScore.ScoreTotal;
+        }
+
+        // Get current score as object.
+        public Score GetScoreObject()
+        {
+            return BattleEngine.BattleScore;
+        }
+
+        // Get number of rounds.
+        public int GetRoundCount()
+        {
+            return BattleEngine.BattleScore.RoundCount;
+        }
+
+        // Get Battle results as string.
+        public string GetResultsOutput()
+        {
+            string myResult = null;
+                    //"Battle Ended" + BattleEngine.BattleScore.ScoreTotal +
+                    //" Total Score :" + BattleEngine.BattleScore.ExperienceGainedTotal +
+                    //" Total Experience :" + BattleEngine.BattleScore.ExperienceGainedTotal +
+                    //" Rounds :" + BattleEngine.BattleScore.RoundCount +
+                    //" Turns :" + BattleEngine.BattleScore.TurnCount +
+                    //" Monster Kills :" + BattleEngine.BattleScore.MonstersKilledList;
+
+            Debug.WriteLine(myResult);
+
+            return myResult;
+        }
+    }
+}
--- Crawl/Crawl/Views/Battle/AutoBattlePage.xaml.cs
+++ Crawl/Crawl/Views/Battle/AutoBattlePage.xaml.cs
@@ -22,38 +22,32 @@ namespace Crawl.Views.Battle
         private async void AutoBattleButton_Command(object sender, EventArgs e)
         {
             // Can create a new battle engine...
-            var myBattleEngine = new BattleEngine();
+            var myEngine = new AutoBattleEngine();
 
-            var result = myBattleEngine.AutoBattle();
+            var result = myEngine.ExecuteAutoBattle();
 
             if (result == false)
             {
-                var answer = await DisplayAlert("Error", "No Characters to battle with", "OK","Cancel");
-                if (answer)
-                {
-                    var a = 1;
-                    // Can't run auto battle, no characters...
-                }
+                await DisplayAlert("Error", "No Characters Avaialbe", "OK");
+                return;
             }
 
-            if (myBattleEngine.BattleScore.RoundCount < 1)
+            if (myEngine.GetRoundCount() < 1)
             {
-                var answer = await DisplayAlert("Error", "No Rounds Fought", "OK", "Cancel");
-                if (answer)
-                {
-                    var a = 1;
-                    // Can't run auto battle, no characters...
-                }
+                await DisplayAlert("Error", "No Rounds Fought", "OK");
+                return;
             }
 
-            var outputString = "Battle Over! Score " + myBattleEngine.BattleScore.ScoreTotal;
-            var action = await DisplayActionSheet(outputString, 
-                "Cancel", 
-                null, 
-                "View Score");
+            var myResult = myEngine.GetResultsOutput();
+            var myScore = myEngine.GetScoreValue();
+
+            var outputString = "Battle Over! Score " + myScore.ToString();
+
+            var action = await DisplayActionSheet(outputString, "Cancel", null, "View Score");
             if (action == "View Score")
             {
-                await Navigation.PushAsync(new ScoreDetailPage(new ScoreDetailViewModel(myBattleEngine.BattleScore)));
+                var myScoreObject = myEngine.GetScoreObject();
+                await Navigation.PushAsync(new ScoreDetailPage(new ScoreDetailViewModel(myScoreObject)));
             }
         }
     }
