0c253866efdf65fd34b32d44f58aee85608e0578
ThomasChin <tchin10@outlook.com>
2019-03-01 16:45:46 -0800

    Add Battle Screen files and format UX for OpeningPage

---
+++ Crawl/Crawl/Views/Battle/BattleOverPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Crawl.Views.Battle
+{
+    public class BattleOverPage : ContentPage
+    {
+        public BattleOverPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
