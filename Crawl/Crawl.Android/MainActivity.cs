5d924895c1d9e60e99687f90adf9a2cfac7adce4
FisherHarris <fisherpatrickharris@gmail.com>
2019-03-11 14:50:32 -0700

    Added splash screen, icon, changed name to horn wars instead of CRAWL

--- Crawl/Crawl.Android/MainActivity.cs
+++ Crawl/Crawl.Android/MainActivity.cs
@@ -9,11 +9,16 @@ using Android.OS;
 
 namespace Crawl.Droid
 {
-    [Activity(Label = "Crawl", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Horn Wars", Theme = "@style/splashscreen", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleTop)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            // Name of the MainActivity theme you had there before.
+            // Or you can use global::Android.Resource.Style.ThemeHoloLight
+            base.SetTheme(Resource.Style.MainTheme);
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
