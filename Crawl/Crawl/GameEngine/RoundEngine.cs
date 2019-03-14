cec2b27a414c17753892c7c4a839de13730a5db4
Mike Koenig <mikekoenig@live.com>
2019-03-13 19:44:35 -0700

    Added the TimeWarp Message to Round Message

--- Crawl/Crawl/GameEngine/RoundEngine.cs
+++ Crawl/Crawl/GameEngine/RoundEngine.cs
@@ -269,6 +269,7 @@ namespace Crawl.GameEngine
                     // TimeWarp Enabled, need to show in output so set the flag
                     GameGlobals.TimeWarpEnabled = true;
                     Console.WriteLine("Time Warp Happens... Slow is Fast");
+                    BattleMessages.RoundMessage = "Time Warp Happens... Slow is Fast";
 
                     // If enabled, switch to slowest first...
                     PlayerList = PlayerList.OrderBy(a => a.Speed)
